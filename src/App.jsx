1e0ea6ca9c0e5257bd10ae0fe5c56e4aa922145b
Zahid de Jesus Avalos Hernandez <l19350283@tuctepec.tecnm.mx>
2023-03-27 09:52:35 -0600

    control de inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import { useState } from "react";
 
 function App() {
-  const [inputValue,setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState({
+    input1: "",
+    input2: "",
+  });
   const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-    console.log(e.target.value);
+    setInputValue({
+      ...inputValue,
+      [e.target.name]: e.target.value
+    });
   }
 
   return (
-    <div style={{textAlign: "center"}}>
+    <div style={{ textAlign: "center" }}>
       <h1>Input Test</h1>
       <hr />
+      <label htmlFor="input1">Input1</label>
       <input
-      onChange={(e)=>handleInputChange(e)}
+        id="input1"
+        name="input1"
+        onChange={(e) => handleInputChange(e)}
         type="text"
-        value={inputValue}
+        value={inputValue.input1}
       />
+      <br />
+      <label>
+        Input2
+        <input
+          name="input2"
+          className="mt-2"
+          onChange={(e) => handleInputChange(e)}
+          type="text"
+          value={inputValue.input2}
+        />
+      </label>
     </div>
 
   )
